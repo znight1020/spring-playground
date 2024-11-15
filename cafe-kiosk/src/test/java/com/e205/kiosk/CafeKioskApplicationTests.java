0e99bfa74cf9f070c7cdbc2d5df147d36b2cc255
이현수 <znight1020@naver.com>
2024-11-15 16:04:16 +0900

    setup cafe-kiosk project

---
+++ cafe-kiosk/src/test/java/com/e205/kiosk/CafeKioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e205.kiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeKioskApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
